97e96f2f3d120123e79ed84e3b546debda3ba663
sayedahmadnawid <sayed_sayedzada@yahoo.com>
2022-08-04 17:26:00 +0900

    added the button component

--- resources/js/components/layout/TheLeftNavBar.vue
+++ resources/js/components/layout/TheLeftNavBar.vue
@@ -1,95 +1,202 @@
 <template>
- <TransitionRoot as="template" :show="sidebarOpen">
-      <Dialog as="div" class="relative z-40 md:hidden" @close="sidebarOpen = false">
-        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100" leave-to="opacity-0">
-          <div class="fixed inset-0 bg-gray-600 bg-opacity-75" />
-        </TransitionChild>
+    <TransitionRoot as="template" :show="sidebarOpen">
+        <Dialog
+            as="div"
+            class="relative z-40 md:hidden"
+            @close="sidebarOpen = false"
+        >
+            <TransitionChild
+                as="template"
+                enter="transition-opacity ease-linear duration-300"
+                enter-from="opacity-0"
+                enter-to="opacity-100"
+                leave="transition-opacity ease-linear duration-300"
+                leave-from="opacity-100"
+                leave-to="opacity-0"
+            >
+                <div class="fixed inset-0 bg-gray-600 bg-opacity-75" />
+            </TransitionChild>
 
-        <div class="fixed inset-0 flex z-40">
-          <TransitionChild as="template" enter="transition ease-in-out duration-300 transform" enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0" leave-to="-translate-x-full">
-            <DialogPanel class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-gray-800">
-              <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
-                <div class="absolute top-0 right-0 -mr-12 pt-2">
-                  <button type="button" class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" @click="sidebarOpen = false">
-                    <span class="sr-only">Close sidebar</span>
-                    <XIcon class="h-6 w-6 text-white" aria-hidden="true" />
-                  </button>
+            <div class="fixed inset-0 flex z-40">
+                <TransitionChild
+                    as="template"
+                    enter="transition ease-in-out duration-300 transform"
+                    enter-from="-translate-x-full"
+                    enter-to="translate-x-0"
+                    leave="transition ease-in-out duration-300 transform"
+                    leave-from="translate-x-0"
+                    leave-to="-translate-x-full"
+                >
+                    <DialogPanel
+                        class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-gray-800"
+                    >
+                        <TransitionChild
+                            as="template"
+                            enter="ease-in-out duration-300"
+                            enter-from="opacity-0"
+                            enter-to="opacity-100"
+                            leave="ease-in-out duration-300"
+                            leave-from="opacity-100"
+                            leave-to="opacity-0"
+                        >
+                            <div class="absolute top-0 right-0 -mr-12 pt-2">
+                                <button
+                                    type="button"
+                                    class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                                    @click="sidebarOpen = false"
+                                >
+                                    <span class="sr-only">Close sidebar</span>
+                                    <XIcon
+                                        class="h-6 w-6 text-white"
+                                        aria-hidden="true"
+                                    />
+                                </button>
+                            </div>
+                        </TransitionChild>
+                        <div class="flex-shrink-0 flex items-center px-4">
+                            <img
+                                class="h-8 w-auto"
+                                src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                                alt="Workflow"
+                            />
+                        </div>
+                        <div class="mt-5 flex-1 h-0 overflow-y-auto">
+                            <nav class="px-2 space-y-1">
+                                <a
+                                    v-for="item in navigation"
+                                    :key="item.name"
+                                    :href="item.href"
+                                    :class="[
+                                        item.current
+                                            ? 'bg-gray-900 text-white'
+                                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                        'group flex items-center px-2 py-2 text-base font-medium rounded-md',
+                                    ]"
+                                >
+                                    <component
+                                        :is="item.icon"
+                                        :class="[
+                                            item.current
+                                                ? 'text-gray-300'
+                                                : 'text-gray-400 group-hover:text-gray-300',
+                                            'mr-4 flex-shrink-0 h-6 w-6',
+                                        ]"
+                                        aria-hidden="true"
+                                    />
+                                    {{ item.name }}
+                                </a>
+                            </nav>
+                        </div>
+                    </DialogPanel>
+                </TransitionChild>
+                <div class="flex-shrink-0 w-14" aria-hidden="true">
+                    <!-- Dummy element to force sidebar to shrink to fit close icon -->
                 </div>
-              </TransitionChild>
-              <div class="flex-shrink-0 flex items-center px-4">
-                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />
-              </div>
-              <div class="mt-5 flex-1 h-0 overflow-y-auto">
-                <nav class="px-2 space-y-1">
-                  <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']">
-                    <component :is="item.icon" :class="[item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'mr-4 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
-                    {{ item.name }}
-                  </a>
-                </nav>
-              </div>
-            </DialogPanel>
-          </TransitionChild>
-          <div class="flex-shrink-0 w-14" aria-hidden="true">
-            <!-- Dummy element to force sidebar to shrink to fit close icon -->
-          </div>
-        </div>
-      </Dialog>
+            </div>
+        </Dialog>
     </TransitionRoot>
 
     <!-- Static sidebar for desktop -->
     <div class="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
-      <!-- Sidebar component, swap this element with another sidebar if you like -->
-      <div class="flex-1 flex flex-col min-h-0 bg-gray-800">
-        <div class="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />
+        <!-- Sidebar component, swap this element with another sidebar if you like -->
+        <div class="flex-1 flex flex-col min-h-0 bg-gray-800">
+            <div class="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
+                <img
+                    class="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    alt="Workflow"
+                />
+            </div>
+            <div class="flex-1 flex flex-col overflow-y-auto">
+                <nav class="flex-1 px-2 py-4 space-y-1">
+                    <a
+                        v-for="item in navigation"
+                        :key="item.name"
+                        :href="item.href"
+                        :class="[
+                            item.current
+                                ? 'bg-gray-900 text-white'
+                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            'group flex items-center px-2 py-2 text-sm font-medium rounded-md',
+                        ]"
+                    >
+                        <component
+                            :is="item.icon"
+                            :class="[
+                                item.current
+                                    ? 'text-gray-300'
+                                    : 'text-gray-400 group-hover:text-gray-300',
+                                'mr-3 flex-shrink-0 h-6 w-6',
+                            ]"
+                            aria-hidden="true"
+                        />
+                        {{ item.name }}
+                    </a>
+                </nav>
+            </div>
         </div>
-        <div class="flex-1 flex flex-col overflow-y-auto">
-          <nav class="flex-1 px-2 py-4 space-y-1">
-            <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']">
-              <component :is="item.icon" :class="[item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'mr-3 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
-              {{ item.name }}
-            </a>
-          </nav>
-        </div>
-      </div>
     </div>
     <!-- Sidebar End-->
 </template>
 
-<script setup>
-import { ref } from 'vue'
+<script>
 import {
-  Dialog,
-  DialogPanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  TransitionChild,
-  TransitionRoot,
-} from '@headlessui/vue'
+    Dialog,
+    DialogPanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    TransitionChild,
+    TransitionRoot,
+} from "@headlessui/vue";
 
 import {
-  BellIcon,
-  CalendarIcon,
-  ChartBarIcon,
-  FolderIcon,
-  HomeIcon,
-  InboxIcon,
-  MenuAlt2Icon,
-  UsersIcon,
-  XIcon,
-} from '@heroicons/vue/outline'
-import { SearchIcon } from '@heroicons/vue/solid'
+    BellIcon,
+    CalendarIcon,
+    ChartBarIcon,
+    FolderIcon,
+    HomeIcon,
+    InboxIcon,
+    MenuAlt2Icon,
+    UsersIcon,
+    XIcon,
+} from "@heroicons/vue/outline";
+import { SearchIcon } from "@heroicons/vue/solid";
 
-const navigation = [
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
-]
-
-const sidebarOpen = ref(false)
+export default {
+    data() {
+        return {
+            sidebarOpen: false,
+            navigation: [
+                { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
+                { name: "Team", href: "#", icon: UsersIcon, current: false },
+                {
+                    name: "Projects",
+                    href: "#",
+                    icon: FolderIcon,
+                    current: false,
+                },
+                {
+                    name: "Calendar",
+                    href: "#",
+                    icon: CalendarIcon,
+                    current: false,
+                },
+                {
+                    name: "Documents",
+                    href: "#",
+                    icon: InboxIcon,
+                    current: false,
+                },
+                {
+                    name: "Reports",
+                    href: "#",
+                    icon: ChartBarIcon,
+                    current: false,
+                },
+            ],
+        };
+    },
+};
 </script>
--- resources/js/pages/projects/ProjectList.vue
+++ resources/js/pages/projects/ProjectList.vue
@@ -8,7 +8,7 @@
         <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>
       </div>
       <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-        <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add user</button>
+        <base-button>Add Project</base-button>
       </div>
     </div>
     <div class="mt-8 flex flex-col">
@@ -49,9 +49,14 @@
   </base-content>
 </template>
 
-<script setup>
-const people = [
-  { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-  // More people...
-]
+<script>
+export default{
+    data(){
+        return {
+            people: [
+                { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' }
+            ]
+        }
+    }
+}
 </script>
