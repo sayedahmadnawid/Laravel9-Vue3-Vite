6251f8fe97cce6db932e5cb96bff1dbd69544308
sayedahmadnawid <sayed_sayedzada@yahoo.com>
2022-08-04 14:03:42 +0900

    layout v.1

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,10 +3,11 @@ import './bootstrap';
 import { createApp } from 'vue'
 import router from './router.js'
 import App from './App.vue'
-
+import BaseContent from './components/ui/BaseContent.vue'
 
 const app = createApp(App)
 
 app.use(router)
+app.component('base-content', BaseContent)
 
 app.mount("#app")
